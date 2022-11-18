be5bf75e722bfb838086484e2412d6c551dc5fc7
Hussein Smith <husseinshakir81@gmail.com>
2022-11-18 15:30:27 +0300

    added ability to send reports to Discord server

---
+++ SReporter/src/main/java/smith/lib/net/reporter/DiscReporter.java
@@ -0,0 +1,12 @@
+package smith.lib.net.reporter;
+
+import android.content.Context;
+
+public class DiscReporter {
+    
+    private Context context;
+    
+    public DiscReporter(Context context) {
+        this.context = context;
+    }
+}
